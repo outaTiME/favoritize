45745fae93fac0df90ec536f7b9b4318264242e0
outaTiME <afalduto@gmail.com>
2012-04-18 23:47:53 -0300

    Add search module using aws product advertising.

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var
   express = require('express'),
   everyauth = require('everyauth'),
   routes = require('./routes'),
-  aws = require('aws-lib'),
 
   /** Yay, out application name. */
   app_name = "Favoritize",
@@ -24,15 +23,6 @@ var
 
   /** Invitation code, only available in development mode. **/
   invitation_code = "1515d5b65c8b446d6c152395d7d484bc",
-
-  /** Amazon access key id. **/
-  aws_access_key_id = "AKIAJNP2M6VLA27BZ4RA",
-
-  /** Amazon secret access key. **/
-  aws_secret_access_key = "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",
-
-  /** Amazon associate tag (only for product advertising api). **/
-  aws_associate_tag = "favoritize-20",
 
   /** Get value only if running app in development mode, if not return empty (used by helpers). **/
   getEnvironmentValue = function(value, empty) {
@@ -202,6 +192,10 @@ app.dynamicHelpers({
 app.get('/', checkAuth, routes.home);
 app.get('/test', checkAuth, routes.test);
 
+// ajax
+
+app.post('/search', checkAuth, routes.search);
+
 // environment specific
 
 app.configure('development', function(){
@@ -211,20 +205,6 @@ app.configure('development', function(){
 app.configure('production', function(){
   app.use(express.errorHandler());
 });
-
-// aws test
-
-/*
-var prodAdv = aws.createProdAdvClient("AKIAJNP2M6VLA27BZ4RA", "2tr4x5lgpnq0QLUdSB8zjidgMfAUXFHNa3zjcSZ0",
-  "favoritize-20");
-
-prodAdv.call("ItemSearch", {SearchIndex: "All", Keywords: "node%20js"}, function(err, result) {
-  console.log(JSON.stringify(result.Items.TotalResults));
-  result.Items.Item.forEach(function(item) {
-    console.log("Item: %s - data: %j", item.ItemAttributes.Title, item.ItemAttributes);
-  });
-});
-*/
 
 // launcher
 
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,53 @@
 
-/*
- * GET home page.
+/**
+ * Module dependencies.
  */
 
-exports.home = function(req, res){
+var
+
+  aws = require('aws-lib'),
+
+  /** Amazon access key id. **/
+  aws_access_key_id = "AKIAJNP2M6VLA27BZ4RA",
+
+  /** Amazon secret access key. **/
+  aws_secret_access_key = "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",
+
+  /** Amazon associate tag (only for product advertising api). **/
+  aws_associate_tag = "favoritize-20",
+
+  /** Return new AWS in each call. */
+  getProductAdvertisingClient = function () {
+    return aws.createProdAdvClient(aws_access_key_id, aws_secret_access_key, aws_associate_tag);
+  }
+
+// routes
+
+exports.home = function (req, res) {
   res.render('home', {
   })
 };
 
-exports.test = function(req, res){
+exports.test = function (req, res) {
   res.render('test', {
     title: 'Test'
   })
+};
+
+exports.search = function (req, res) {
+  console.log("%j", req.params);
+  console.log("%j", req.body);
+  var prodAdv = getProductAdvertisingClient(), keywords = req.body["keywords"], items = [];
+  console.log("Searching keywords at amazon: %s", keywords);
+  prodAdv.call("ItemSearch", {SearchIndex: "All", Keywords: keywords}, function(err, result) {
+    if (!err) {
+      items = result.Items.Item;
+    }
+    console.log('Items: %j', items);
+    res.partial("search", {
+      items: items
+    });
+  });
+
 };
 
